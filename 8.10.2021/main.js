168ce03ca89fe863789df4bb55506e6295ea009a
segedu <seggedu20@gmail.com>
2021-10-10 01:40:20 +0300

    assignment 7.10.2021

--- 8.10.2021/main.js
+++ 8.10.2021/main.js
@@ -1,27 +1,26 @@
 const api_key = "641974375fb31ab9962b7f26bce1a2a0";
 const cordCont = document.getElementById("cordCont");
+let longtitude = document.getElementById("longtitude");
+let latitude = document.getElementById("latitude");
 let btn = document.getElementById("btn");
-let cordinates = document.getElementById("cordinates");
-let cordValue = cordinates.value;
 
-function fetchFunc(cordinate) {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cordinate}&appid=${api_key}`,
-    {
-      method: "GET",
-    }
-  )
+function print() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${api_key}`;
+  fetch(url, {
+    method: "GET",
+  })
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
-      if (response.code == 404) {
-        `<h1>${response.message}</h1>`;
+      if (data.cod == 400) {
+        cordCont.innerHTML += `<h1>${data.message}</h1>`;
       }
-      cordCont.innerHTML += `${response.main.cordinate}`;
+      cordCont.innerHTML += `${response.main.coordinates}`;
     })
     .catch((error) => console.log(error));
 }
-fetchFunc("london");
-btn.onclick = fetchFunc(cordValue);
+btn.addEventListener("click", () => {
+  print(longtitude.value, latitude.value, cordCont);
+});
