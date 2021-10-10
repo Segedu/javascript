07ae1ab0644931c9bdb49fe86045a02f1635eb06
segedu <seggedu20@gmail.com>
2021-10-10 16:26:38 +0300

    third party class assignment

---
+++ 10.10.2021/main.js
@@ -0,0 +1,49 @@
+//!1
+// let name1 = "new week";
+// console.log(name1);
+
+//!2
+// console.log("hello world");
+
+//!3
+// for (let index = 0; index < 5; index++) {
+//   console.log("hello world");
+// }
+//!4
+// function sum(num1, num2) {
+//   num1 + num2;
+// }
+// console.log(sum(2, 6));
+
+//!5
+// const os = require("os");
+// console.log(os.platform("win32"));
+
+//!6
+// const fs = require("fs");
+// fs.readFile("./10.10.2021/class.txt", "utf8", (error, data) => {
+//   if (error) {
+//     console.log(error);
+//   }
+//   console.log(data);
+// });
+
+//!7
+// fs.readFileSync()
+
+//!8
+// fs.appendFile("./10.10.2021/class.txt", "lorem", (error) => {
+//   if (error) {
+//     console.log(error);
+//   }
+//   console.log(data);
+// });
+
+//!9
+// fs.appendFileSync();
+
+//!10
+// const chalk = require("chalk");
+// console.log(chalk.blue("segedu"));
+// console.log(chalk.green("thank you jacob"));
+// console.log(chalk.keyword("orange")("Yay for orange colored text!"));
