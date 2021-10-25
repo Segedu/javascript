d2b5e0a7f60a63ffdac3739d88a192a029968595
segedu <seggedu20@gmail.com>
2021-10-25 21:25:42 +0300

    double

---
+++ 25.10.2021/src/server.js
@@ -0,0 +1,13 @@
+const express = require("express"),
+  hbs = require("hbs"),
+  path = require("path"),
+  publicPath = path.join(__dirname, "..", "public"),
+  viewsPath = path.join(__dirname, "..", "templates", "views"),
+  partialsPath = path.join(__dirname, "..", "templates", "partials"),
+  app = express(),
+  PORT = 8080;
+
+app.use(express.static(publicPath));
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+// app.("", partialsPath);
